60661b9dfeada39a5a741b073433b6c70865efb9
OnigiriJPN <yuzuhajintian@gmail.com>
2026-07-18 10:48:37 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,27 @@
       <div v-for="(msg, i) in messages" :key="i" class="message-bubble">{{ msg.text }}</div>
     </div>
   </div>
+  <div class="input-area">
+    <input
+      v-model="newMessage"
+      @keyup.enter="handleSend"
+      placeholder="メッセージを入力"
+      class="win11-input"
+      />
+    <button @click="handleSend" class="win11-button">送信📤</button>
+  </div>
 </template>
+<style scoped>
+  .chat-container {
+    display: flex;
+    flex-direction: column;
+    height: 90vh;
+    max-width: 600px;
+    margin: 0 auto;
+    background: #f3f3f3;
+  }
+</style>
+
 <script setup>
 import { ref, onMounted } from 'vue';
 
